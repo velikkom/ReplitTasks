4b1b0710b91cbfb851b53cff9081b5b8f8f01049
Veli Kara <velikkom@hotmail.com>
2022-12-18 14:01:47 +0300

    new question 12/18/2022

---
+++ src/ReplitTasks/NonDuplicate.java
@@ -0,0 +1,58 @@
+package ReplitTasks;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NonDuplicate {
+
+    public static void main(String[] args) {
+        //DO NOT TOUCH BELOW:
+        Scanner input = new Scanner(System.in);
+        int[] nums = {input.nextInt(), input.nextInt(), input.nextInt(),
+                input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt()};
+
+        //WRITE YOUR CODE BELOW:
+
+        int count = 0;
+        String str = "";
+        for (int each : nums) {
+            for (int each2 : nums) {
+
+
+
+
+
+            }
+
+        }
+        System.out.println(str);
+    }
+
+}
+     /*
+### Given an `int []` `nums` that has duplicate numbers,
+ which means each numbers is found twice in the array.
+ Find and print the non-duplicate number,
+ which mean the ones that does not have two occurrences of it.
+ If there is more than one, return the first unique in array!
+
+Main topics: arrays, primitive datatypes, index, operators, loops, if statements, counter
+
+Example:
+```
+Input:
+  [1, 2, 3, 4, 3, 4, 1]
+
+Output:
+  2
+```
+
+```
+Input:
+  [6, 44, 6, 4, 3, 4, 1]
+
+Output:
+  44 (more than one unique number. but 44 is the first)
+```
+
+ */
